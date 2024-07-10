6e63d6635d9ee5c984fddfcada19fd03683b1185
Tan Chun Siong <dreamtcs@hotmail.com>
2024-07-10 13:25:48 +0800

    added shared 2nd camera demos, and renamed main project

---
+++ VSDK_Share2ndCameraAsMultiCam/README.md
@@ -0,0 +1,127 @@
+# VSDK_RawDataDemos
+
+This current sample app is based on version 1.8.10
+A Windows C++ Application demonstrate Zoom Video SDK raw data access in Session
+
+# Install vcpkg for adding dependency libs.
+You might need to use Powershell (as administrator) or Windows Terminal to execute the sh script files
+```
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg integrate install --vcpkg-root c:\vcpkg
+
+```
+
+# Add dependency libs
+opencv might not be necessary if you are not going to do video/image processing before saving.
+opencv might not be necessary if you are just saving raw audio to file..
+opencv will take a while (10 mins) to complete
+
+```
+./vcpkg install jsoncpp
+./vcpkg install opencv 
+```
+
+
+This project is in the VSDK_SkeletonDemo folder
+
+Within `main_frame.cpp` you will need to enter your credentials in the `JoinSession` method
+
+```
+void CMainFrame::JoinSession()
+
+		std::wstring session_name = L"webchun6871";
+		std::wstring sUserName = L"vsdk_skeletondemo";
+		std::wstring session_password_ = L"12345678";
+		std::wstring token = L"xxxxx.yyyyyy.zzzzz";
+```
+
+
+## Download the official Video SDK for Windows, and add sdk files into a folder name `SDK`
+
+The folder structure should look something like this
+
+VSDK_SkeletonDemo
+- SDK    <- #add sdk files into this directory
+	-x86
+	-x64
+- x64
+	-Debug
+	-Release
+
+
+## Open and Run Project
+
+Open "VSDK_RawDataDemos.sln" file from Visual Studio 2022.
+
+Hit F5 or click from menu "Debug" -> "Start Debugging" in x86 or x64 to launch the application.
+
+
+
+
+  # Error
+
+  what if i would like to use x64 environment?
+
+  add this to your environment variable before installing openCV from vcpkg
+
+  VCPKG_DEFAULT_TRIPLET = x64-windows
+
+  and reinstall
+
+  ```
+  ./vcpkg install jsoncpp
+  ./vcpkg install opencv
+  ```
+
+  There are some errors about opencv being unable to find certain libraries?
+
+  They should not affect the basic functionality of this sample, but you can do a list of additional opencv libraries to install
+  ```
+  ./cvpkg list opencv
+  ```
+
+  to install addition libraries, do something like this. This might take around 1 hour, depending on the number of libraries you install
+  ```
+  ./vcpkg install opencv[contrib,ffmpeg,nonfree,opengl,openmp,world]
+  ```
+## Getting Started
+
+The main method, or main entry point of this application is at `main_frame.cpp`
+
+From a high level point of view it will showcase some of the functionalities below.
+For ease of development understanding, I've used the below variables as "control keywords" in `main_frame.cpp` Search for these "control keywords" (bool variables) to understand the flow
+
+Example searching for "control keyword" `getRawAudio` will highlight bookmark on 
+ - header files which needs to be included, 
+ - the session context options needed before joining a session, 
+ - the methods to call after successfully joining a session and 
+ - the callbacks to handle the raw audio frame
+
+```
+//these are list of "controls keywords' to demonstrate the flow
+bool getRawAudio = false;
+bool getRawVideo = false;
+bool getRawShare = false;
+bool sendRawVideo = false;
+bool sendRawAudio = false;
+bool sendRawShare = false;
+bool enableLTT = false;
+bool getLang = false;
+bool enableCloudRecording = false;
+bool enableCallout = false;
+bool previewCameraAndMicrophone = true;
+```
+
+
+
+# Upgrading Guide
+
+You will need to download the latest Video SDK Windows for c++ from marketplace.zoom.us
+
+Replace the files in the folder `SDK` with those found in the SDK files downloaded from marketplace.zoom.us
+
+You will need to ensure any missing abstract classes are implemented etc... before you can compile and upgrade to a newer SDK version.
+Usually newer version of SDK will introduce new interfaces / abstract classes.
+
---
+++ VSDK_Share2ndCameraAsMultiCam/main_frame.cpp
@@ -0,0 +1,441 @@
+#include <windows.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <fstream>
+#include <iosfwd>
+#include <iostream>
+#include <signal.h>
+#include <stdlib.h>
+
+
+#include "main_frame.h";
+#include "helpers/zoom_video_sdk_user_helper_interface.h"
+#include "zoom_video_sdk_api.h"
+#include "zoom_video_sdk_def.h"
+#include "zoom_video_sdk_delegate_interface.h"
+#include "zoom_video_sdk_interface.h"
+
+#include "zoom_video_sdk_platform.h"
+
+#include "zoom_video_sdk_delegate_interface.h"
+#include "zoom_video_sdk_session_info_interface.h"
+#include "helpers/zoom_video_sdk_audio_helper_interface.h"
+#include "helpers/zoom_video_sdk_video_helper_interface.h"
+#include "helpers/zoom_video_sdk_share_helper_interface.h"
+#include "helpers/zoom_video_sdk_chat_helper_interface.h"
+#include "helpers/zoom_video_sdk_cmd_channel_interface.h"
+#include "zoom_video_sdk_interface.h"
+#include "zoom_video_sdk_api.h"
+#include "helpers/zoom_video_sdk_user_helper_interface.h"
+#include "zoom_video_sdk_platform.h"
+#include <vector>
+
+//needed for audio
+//#include "ZoomVideoSDKVirtualAudioMic.h"
+//#include "ZoomVideoSDKVirtualAudioSpeaker.h"
+#include "helpers/zoom_video_sdk_audio_send_rawdata_interface.h"
+
+//needed for share screen
+//#include "ZoomVideoSDKShareSource.h"
+#include "helpers/zoom_video_sdk_share_helper_interface.h"
+
+//needed for get raw video
+//#include "ZoomVideoSDKRawDataPipeDelegate.h"
+//#include "ZoomVideoSDKVideoSource.h"
+#include "helpers/zoom_video_sdk_video_helper_interface.h"
+
+//needed for command channel
+#include "helpers/zoom_video_sdk_cmd_channel_interface.h"
+
+//needed for chat
+#include "helpers/zoom_video_sdk_chat_helper_interface.h"
+#include "zoom_video_sdk_chat_message_interface.h"
+
+//needed for LTT
+#include "zoom_video_sdk_session_info_interface.h"
+#include <string>
+
+#include "json\json.h"
+#include "WebService.h"
+#include <atlstr.h>
+
+USING_ZOOM_VIDEO_SDK_NAMESPACE
+using namespace Json;
+using namespace std;
+
+wstring sdk_jwt;
+wstring sessionName;;
+wstring password;
+IZoomVideoSDK* video_sdk_obj_;
+constexpr auto CONFIG_FILE = "config.json";
+bool isJWTWebService = true;
+
+
+
+wstring StringToWString(string input)
+{
+	wstring output;
+	output.assign(input.begin(), input.end());
+	return output;
+}
+
+string WStringToString(wstring input)
+{
+	string output;
+	output.assign(input.begin(), input.end());
+	return output;
+}
+
+wstring QuestionInput(string qustion)
+{
+	wstring input;
+	std::cout << qustion;
+	getline(wcin, input);
+	return input;
+}
+/// <summary>
+/// Loads configuration from config.json file.
+/// This config file contains the JWT token, meeting number, passcode and mp4 file for streaming
+/// </summary>
+void LoadConfig() {
+	ifstream f(CONFIG_FILE);
+	Value config;
+	printf("Trying to find meeting parameters from config.json.\n");
+	bool isConfigFileOpened = f.is_open();
+	if (isConfigFileOpened) {
+		try {
+			f >> config;
+		}
+		catch (exception) {
+			printf("config.json is in wrong format.\n");
+		}
+	}
+	else {
+		printf("Didn't find config.json file.\n");
+	}
+	if (!isConfigFileOpened || config["sdk_jwt"].empty() || config["sdk_jwt"].asString() == "") {
+		sdk_jwt = QuestionInput("SDK JWT: ");
+	}
+	else {
+		sdk_jwt = StringToWString(config["sdk_jwt"].asString());
+		printf("Found \"SDK JWT\" from %s: \n\"%s\"\n", CONFIG_FILE, WStringToString(sdk_jwt).c_str());
+	}
+	bool toQuestionForMeetingNumber = false;
+	if (!isConfigFileOpened || config["sessionName"].empty() || config["sessionName"].asString() == "")
+		toQuestionForMeetingNumber = true;
+	else {
+		try {
+			sessionName = StringToWString(config["sessionName"].asString());
+
+			printf("Found \"sessionName\" from %s: \"%s\"\n", CONFIG_FILE, WStringToString(sessionName).c_str());
+		}
+		catch (exception)
+		{
+			try {
+				sessionName = StringToWString(config["sessionName"].asString());
+
+				printf("Found \"sessionName\" from %s: \"%s\"\n", CONFIG_FILE, WStringToString(sessionName).c_str());
+			}
+			catch (exception)
+			{
+				printf("Failed to read \"sessionName\" from config.json, it should include only numbers.\n");
+				toQuestionForMeetingNumber = true;
+			}
+		}
+	}
+	while (toQuestionForMeetingNumber) {
+		std::wcout << "SessionName: ";
+		string input;
+		getline(cin, input);
+		try {
+			sessionName = stoull(input, nullptr, 10);
+			toQuestionForMeetingNumber = false;
+		}
+		catch (exception)
+		{
+			printf("Session Name should include numbers.\n");
+		}
+	}
+	if (!isConfigFileOpened || config["password"].empty() || config["password"].asString() == "") {
+		printf("Password is empty.\n");
+	}
+	else {
+		password = StringToWString(config["password"].asString());
+		printf("Found \"password\" from %s: \"%s\"\n", CONFIG_FILE, WStringToString(password).c_str());
+	}
+
+}
+
+MainFrame::MainFrame() {
+
+}
+
+MainFrame::~MainFrame()
+{
+}
+
+// this callback happens when sdk / use has successfully joined the session
+void MainFrame::onSessionJoin()
+{
+	std::cout << "onSessionJoin()" << std::endl;
+
+
+	if (video_sdk_obj_)
+	{
+		//dreamtcs
+		//starting point
+
+
+		IZoomVideoSDKVideoHelper* videohelper = video_sdk_obj_->getVideoHelper();
+
+
+		if (videohelper) {
+
+			//set preferences
+			ZoomVideoSDKVideoPreferenceSetting setting;
+			setting.mode = ZoomVideoSDKVideoPreferenceMode_Balance;
+			//setting.minimum_frame_rate = 20;
+			//setting.maximum_frame_rate = 30;
+			videohelper->setVideoQualityPreference(setting);
+
+
+			//get all camera devices
+			IVideoSDKVector<IZoomVideoSDKCameraDevice*>* pCameraList = videohelper->getCameraList();
+			IZoomVideoSDKCameraDevice* usbCaptureDevice{};
+
+			//loop all camera devices
+			for (int i = 0; i < pCameraList->GetCount(); i++)
+			{
+				IZoomVideoSDKCameraDevice* pCamera = pCameraList->GetItem(i);
+
+				CString strItem;
+
+				//check if there is persisted settings (multi-camera send) from last run
+				//if there is, remove the setting
+				if (pCamera->isSelectedAsMultiCamera()) {
+					strItem.Format(_T("%s***%s"), pCamera->getDeviceName(), "true");
+					wprintf(_T("%s\n"), (LPCTSTR)strItem);
+					//disable it
+					bool disableSuccess = videohelper->disableMultiStreamVideo(pCamera->getDeviceId());
+					if (disableSuccess) { wprintf(_T("%s\n"), "disable success"); }
+					else { wprintf(_T("%s\n"), "disable failed"); }
+				}
+				else {
+					strItem.Format(_T("%s***%s"), pCamera->getDeviceName(), "false");
+					wprintf(_T("%s\n"), (LPCTSTR)strItem);
+				}
+
+				//search for USB devicewith the word Link or USB in it's name, and assign it to a temp variable
+				CString deviceName(pCamera->getDeviceName());
+				if (deviceName.Find(_T("Link")) != -1 || deviceName.Find(_T("USB")) != -1) {
+					wprintf(_T("Camera ID for device with 'Link' in the name: %s\n"), (LPCTSTR)pCamera->getDeviceId());
+					usbCaptureDevice = pCamera;
+				}
+
+			}
+
+
+			//if there is USB capture device, send USB capture device as main camera
+			//and send webcam as multicam
+			if (usbCaptureDevice) {
+				videohelper->selectCamera(usbCaptureDevice->getDeviceId());
+				ZoomVideoSDKErrors err = videohelper->startVideo();
+
+				//send webcam as multicam, this is sending camera which is in hardcoded index 0
+
+				//sharehelper->enableOptimizeForSharedVideo(true);
+				int index = 0;
+				IZoomVideoSDKCameraDevice* pCamera = pCameraList->GetItem(index);
+				boolean multistreamSuccess = videohelper->enableMultiStreamVideo(pCamera->getDeviceId(), pCamera->getDeviceName());
+				if (multistreamSuccess) { //return ZoomVideoSDKErrors_Success;
+				}
+				else {// return ZoomVideoSDKErrors_Unknown; 
+				}
+			
+			}
+
+			//if there is no USB Capture device, send webcamp as main cam. This is hardcoded index of 0
+			else {
+				int index = 0;
+				IZoomVideoSDKCameraDevice* cam = videohelper->getCameraList()->GetItem(index);
+				videohelper->selectCamera(cam->getDeviceId());
+				
+			}
+		}
+	}
+
+
+}
+void MainFrame::onSessionLeave() {}
+void MainFrame::onError(ZoomVideoSDKErrors errorCode, int detailErrorCode)
+{
+	std::cout << "onError(), errorCode " << errorCode << " detailedErrorCode: " << detailErrorCode << std::endl;
+
+}
+void MainFrame::onUserJoin(IZoomVideoSDKUserHelper* pUserHelper, IVideoSDKVector<IZoomVideoSDKUser*>* userList)
+{
+	std::cout << "onUserJoin()" << std::endl;
+	//we are using the onUserJoin callback to subscribe to a user's video feed
+	//this is a efficient way of subscription, as there is a limit on the number of video feed which the sdk can subscribe to
+	//for a rule of thumb, you should be able to subscribe up to 4 x 360p video stream per instance of SDK 
+
+}
+void MainFrame::onUserLeave(IZoomVideoSDKUserHelper* pUserHelper, IVideoSDKVector<IZoomVideoSDKUser*>* userList) {}
+void MainFrame::onUserVideoStatusChanged(IZoomVideoSDKVideoHelper* pVideoHelper, IVideoSDKVector<IZoomVideoSDKUser*>* userList) {}
+void MainFrame::onUserAudioStatusChanged(IZoomVideoSDKAudioHelper* pAudioHelper, IVideoSDKVector<IZoomVideoSDKUser*>* userList) {}
+void MainFrame::onUserShareStatusChanged(IZoomVideoSDKShareHelper* pShareHelper, IZoomVideoSDKUser* pUser, ZoomVideoSDKShareStatus status, ZoomVideoSDKShareType type) {}
+void MainFrame::onCalloutJoinSuccess(IZoomVideoSDKUser* pUser, const zchar_t* phoneNumber) {}
+void MainFrame::onUserRecordingConsent(IZoomVideoSDKUser* pUser) {}
+void MainFrame::onLiveStreamStatusChanged(IZoomVideoSDKLiveStreamHelper* pLiveStreamHelper, ZoomVideoSDKLiveStreamStatus status) {}
+void MainFrame::onChatNewMessageNotify(IZoomVideoSDKChatHelper* pChatHelper, IZoomVideoSDKChatMessage* messageItem)
+{
+	std::cout << "onChatNewMessageNotify()" << std::endl;
+	if (!messageItem)
+		return;
+	IZoomVideoSDKUser* send_user = messageItem->getSendUser();
+	if (send_user)
+	{
+		const zchar_t* user_name = send_user->getUserName();
+		const zchar_t* content = messageItem->getContent();
+
+
+	}
+}
+void MainFrame::onUserHostChanged(IZoomVideoSDKUserHelper* pUserHelper, IZoomVideoSDKUser* pUser) {}
+void MainFrame::onUserActiveAudioChanged(IZoomVideoSDKAudioHelper* pAudioHelper, IVideoSDKVector<IZoomVideoSDKUser*>* list) {}
+void MainFrame::onSessionNeedPassword(IZoomVideoSDKPasswordHandler* handler) {}
+void MainFrame::onSessionPasswordWrong(IZoomVideoSDKPasswordHandler* handler) {}
+void MainFrame::onMixedAudioRawDataReceived(AudioRawData* data_) {}
+void MainFrame::onOneWayAudioRawDataReceived(AudioRawData* data_, IZoomVideoSDKUser* pUser) {}
+void MainFrame::onSharedAudioRawDataReceived(AudioRawData* data_) {}
+void MainFrame::onUserManagerChanged(IZoomVideoSDKUser* pUser) {}
+void MainFrame::onUserNameChanged(IZoomVideoSDKUser* pUser) {}
+void MainFrame::onCameraControlRequestResult(IZoomVideoSDKUser* pUser, bool isApproved) {}
+void MainFrame::onCameraControlRequestReceived(IZoomVideoSDKUser* pUser, ZoomVideoSDKCameraControlRequestType requestType, IZoomVideoSDKCameraControlRequestHandler* pCameraControlRequestHandler) {}
+void MainFrame::onCommandReceived(IZoomVideoSDKUser* pSender, const zchar_t* strCmd)
+{
+	/*wstring wstrCmd(strCmd);
+	if (wstrCmd.empty())
+		return;
+
+	std::vector<std::wstring> cmd_elems;
+	split(wstrCmd, _T('|'), cmd_elems);
+
+	if (cmd_elems.size() == 0)
+		return;
+
+	CmdChannelType nCmdType = (CmdChannelType)_ttoi(cmd_elems[0].c_str());
+
+	IParseChannelCmdWnd* channelCmdWnd = NULL;
+	if (cmd_channel_wnd_map_.count(nCmdType) > 0)
+		channelCmdWnd = cmd_channel_wnd_map_[nCmdType];
+
+	if (channelCmdWnd)
+		channelCmdWnd->OnParseChannelCmd(pSender, cmd_elems);*/
+}
+
+void MainFrame::onCommandChannelConnectResult(bool isSuccess) {}
+void MainFrame::onInviteByPhoneStatus(PhoneStatus status, PhoneFailedReason reason) {}
+void MainFrame::onCloudRecordingStatus(RecordingStatus status, IZoomVideoSDKRecordingConsentHandler* pHandler) {}
+void MainFrame::onHostAskUnmute() {}
+void MainFrame::onMultiCameraStreamStatusChanged(ZoomVideoSDKMultiCameraStreamStatus status, IZoomVideoSDKUser* pUser, IZoomVideoSDKRawDataPipe* pVideoPipe) {}
+void MainFrame::onMicSpeakerVolumeChanged(unsigned int micVolume, unsigned int speakerVolume) {}
+void MainFrame::onAudioDeviceStatusChanged(ZoomVideoSDKAudioDeviceType type, ZoomVideoSDKAudioDeviceStatus status) {}
+void MainFrame::onTestMicStatusChanged(ZoomVideoSDK_TESTMIC_STATUS status) {}
+void MainFrame::onSelectedAudioDeviceChanged() {}
+void MainFrame::onCameraListChanged() {}
+void MainFrame::onLiveTranscriptionStatus(ZoomVideoSDKLiveTranscriptionStatus status) {}
+void MainFrame::onLiveTranscriptionMsgReceived(const zchar_t* ltMsg, IZoomVideoSDKUser* pUser, ZoomVideoSDKLiveTranscriptionOperationType type) {}
+void MainFrame::onOriginalLanguageMsgReceived(ILiveTranscriptionMessageInfo* messageInfo) {}
+void MainFrame::onLiveTranscriptionMsgInfoReceived(ILiveTranscriptionMessageInfo* messageInfo) {}
+void MainFrame::onLiveTranscriptionMsgError(ILiveTranscriptionLanguage* spokenLanguage, ILiveTranscriptionLanguage* transcriptLanguage) {}
+void MainFrame::onChatMsgDeleteNotification(IZoomVideoSDKChatHelper* pChatHelper, const zchar_t* msgID, ZoomVideoSDKChatMessageDeleteType deleteBy) {}
+void MainFrame::onChatPrivilegeChanged(IZoomVideoSDKChatHelper* pChatHelper, ZoomVideoSDKChatPrivilegeType privilege) {}
+void MainFrame::onSendFileStatus(IZoomVideoSDKSendFile* file, const FileTransferStatus& status) {}
+void MainFrame::onReceiveFileStatus(IZoomVideoSDKReceiveFile* file, const FileTransferStatus& status) {}
+void MainFrame::onProxyDetectComplete() {}
+void MainFrame::onProxySettingNotification(IZoomVideoSDKProxySettingHandler* handler) {}
+void MainFrame::onSSLCertVerifiedFailNotification(IZoomVideoSDKSSLCertificateInfo* info) {}
+void MainFrame::onUserVideoNetworkStatusChanged(ZoomVideoSDKNetworkStatus status, IZoomVideoSDKUser* pUser) {}
+void MainFrame::onCallCRCDeviceStatusChanged(ZoomVideoSDKCRCCallStatus status) {}
+void MainFrame::onVideoCanvasSubscribeFail(ZoomVideoSDKSubscribeFailReason fail_reason, IZoomVideoSDKUser* pUser, void* handle) {}
+void MainFrame::onShareCanvasSubscribeFail(ZoomVideoSDKSubscribeFailReason fail_reason, IZoomVideoSDKUser* pUser, void* handle) {}
+void MainFrame::onAnnotationHelperCleanUp(IZoomVideoSDKAnnotationHelper* helper) {}
+void MainFrame::onAnnotationPrivilegeChange(IZoomVideoSDKUser* pUser, bool enable) {}
+void MainFrame::onAnnotationHelperActived(void* handle) {}
+
+void MainFrame::InitVideoSDK()
+{
+	LoadConfig();
+
+	if (isJWTWebService) {
+
+		sdk_jwt = StringToWString(GetSignatureFromWebService("https://asdc.cc/video", WStringToString(sessionName), "1"));
+		std::cout << "token from webservice is :" << WStringToString(sdk_jwt).c_str() << std::endl;
+	}
+	std::cout << "InitVideoSDK()" << std::endl;
+	video_sdk_obj_ = ZOOMVIDEOSDK::CreateZoomVideoSDKObj();
+	video_sdk_obj_->addListener(this);
+	ZoomVideoSDKInitParams init_params;
+	init_params.domain = L"https://zoom.us";
+	init_params.enableLog = true;
+	init_params.logFilePrefix = L"zoom_win_video_demo";
+	init_params.videoRawDataMemoryMode = ZoomVideoSDKRawDataMemoryModeHeap;
+	init_params.shareRawDataMemoryMode = ZoomVideoSDKRawDataMemoryModeHeap;
+	init_params.audioRawDataMemoryMode = ZoomVideoSDKRawDataMemoryModeHeap;
+	init_params.enableIndirectRawdata = false;
+	ZoomVideoSDKErrors err = video_sdk_obj_->initialize(init_params);
+	std::cout << "initialization result is :" << err << std::endl;
+}
+
+void MainFrame::UninitVideoSDK()
+{
+	video_sdk_obj_->cleanup();
+	DestroyZoomVideoSDKObj();
+	video_sdk_obj_ = nullptr;
+}
+
+void MainFrame::JoinSession()
+{
+	std::cout << "JoinSession()" << std::endl;
+	//credentials to auth and password & topic name to join specific session
+	std::wstring session_name = sessionName;
+	std::wstring sUserName = L"vsdk_skeletondemo";
+	std::wstring session_password_ = password;
+	std::wstring token = sdk_jwt;
+
+	//is turn off video
+	bool is_video_on = false;
+	//is mute audio
+	bool is_mute_audio = true;
+
+	//session context 
+	ZoomVideoSDKSessionContext session_context;
+	session_context.sessionName = session_name.c_str();
+	session_context.sessionPassword = session_password_.c_str();
+	session_context.userName = sUserName.c_str();
+	session_context.token = token.c_str();
+	session_context.videoOption.localVideoOn = is_video_on;
+	session_context.audioOption.connect = false;
+	session_context.audioOption.mute = is_mute_audio;
+
+	IZoomVideoSDKSession* pSession = video_sdk_obj_->joinSession(session_context);
+	if (pSession)
+	{
+		pSession->getMyself()->GetVideoPipe();
+	}
+}
+
+void MainFrame::LeaveSession(bool end)
+{
+	ZoomVideoSDKErrors err = video_sdk_obj_->leaveSession(end);
+}
+
+
+
+
+
+
+
